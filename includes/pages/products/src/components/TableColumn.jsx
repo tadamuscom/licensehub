b0ab40fa3664a64aadf42ffba98ded8af5ed923b
= <sorinmarta0@gmail.com>
2024-01-16 12:49:13 +0200

    Started working on the contentEditable

--- includes/pages/products/src/components/TableColumn.jsx
+++ includes/pages/products/src/components/TableColumn.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import sanitizeHtml from "sanitize-html"
+import ContentEditable from 'react-contenteditable';
 
 function TableColumn( props ) {
-    return (
-        <td column={ props.column }>
-            { props.data }
-        </td>
-    );
+    const [ columnData, setColumnData ] = useState( props.data );
+    const [ previousData, setPreviousData ] = useState( props.data );
+
+    const onBlur = ( event ) => {
+        console.log( columnData );
+    }
+
+    const onChange = ( event ) => {
+        setColumnData( event.target.value );
+
+        console.log(event.target.value)
+    }
+
+    if( props.column === 'id' || props.column === 'ID' ){
+        return (
+            <td column={ props.column }>
+                { columnData }
+            </td>
+        );
+    }else{
+        return (
+            <td column={ props.column }>
+                <ContentEditable html={ columnData }  onChange={ onChange } onBlur={ onBlur }/>
+            </td>
+        );
+    }
 }
 
 export default TableColumn;
--- includes/pages/products/src/components/TableRow.jsx
+++ includes/pages/products/src/components/TableRow.jsx
@@ -45,7 +45,7 @@ function TableRow(props ) {
     }
 
     return (
-        <tr contentEditable>
+        <tr>
             { columns }
             <td><button className='tada-action-icon tada-delete-btn' onClick={ deleteClick }>Delete</button></td>
         </tr>
