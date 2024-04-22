5130041704b4e55bb72c88e1ef75e0d267d06cdd
Sorin Marta <sorinmarta0@gmail.com>
2024-04-23 00:23:29 +0300

    Rename and relocate

--- webpack.config.js
+++ webpack.config.js
@@ -29,10 +29,16 @@ module.exports = {
 		alias: {
 			...defaultConfig.resolve.alias,
 			'@settings': resolve(__dirname, 'src/Settings'),
+			'@products': resolve(__dirname, 'src/Products'),
+			'@licenses': resolve(__dirname, 'src/Licenses'),
+			'@api': resolve(__dirname, 'src/Api'),
 		},
 	},
 	entry: {
-		'migratemonkey-settings': './src/Settings/index.js',
+		'licensehub-settings': './src/Settings/index.js',
+		'licensehub-products': './src/Products/index.js',
+		'licensehub-licenses': './src/Licenses/index.js',
+		'licensehub-api': './src/Api/index.js',
 	},
 	output: {
 		filename: '[name]-[chunkhash].js',
