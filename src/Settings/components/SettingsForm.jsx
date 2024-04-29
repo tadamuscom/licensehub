e212268a671e300cc71061a69b3e585d5799f633
Sorin Marta <sorinmarta0@gmail.com>
2024-04-29 12:17:17 +0300

    Settings form refactor

--- src/Settings/components/SettingsForm.jsx
+++ src/Settings/components/SettingsForm.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import { Button, FormGroup } from '@global';
 import { FormStatus } from '@global/components/form/FormStatus';
@@ -6,34 +5,20 @@ import { useForms } from '@global/hooks/useForms';
 import { FluentCRM } from '@settings/components/FluentCRM';
 import { Stripe } from '@settings/components/Stripe';
 
-export const SettingsForm = ({ onSubmit }) => {
+export const SettingsForm = () => {
 	const { loading, result, formData, changeFormValue, post } = useForms({
-		stripeIntegration: false,
-		stripePublicKey: '',
-		stripePrivateKey: '',
-		fluentCRMIntegration: false,
+		stripeIntegration: lchb_settings.stripe_integration === 'true',
+		stripePublicKey: lchb_settings.stripe_public_key,
+		stripePrivateKey: lchb_settings.stripe_private_key,
+		fluentCRMIntegration: lchb_settings.fluentcrm_integration === 'true',
 	});
 
-	useEffect(() => {
-		const stripeIntegration = lchb_settings.stripe_integration === 'true';
-		const stripePublicKey = lchb_settings.stripe_public_key;
-		const stripePrivateKey = lchb_settings.stripe_private_key;
-		const stripeFluentCRM = lchb_settings.fluentcrm_integration === 'true';
-
-		changeFormValue('stripeIntegration', stripeIntegration);
-		changeFormValue('stripePublicKey', stripePublicKey);
-		changeFormValue('stripePrivateKey', stripePrivateKey);
-		changeFormValue('fluentCRMIntegration', stripeFluentCRM);
-	}, []);
-
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-
-		await post('/tadamus/lchb/v1/general-settings', lchb_settings.nonce);
-	};
-
 	return (
-		<form onSubmit={handleSubmit}>
+		<form
+			onSubmit={async (e) => {
+				e.preventDefault();
+				await post('/tadamus/lchb/v1/general-settings', lchb_settings.nonce);
+			}}>
 			<Stripe
 				formData={formData}
 				changeFormValue={changeFormValue}
