c086c86ce19fa30bfaa044a86c832e5c0424ec30
Sorin Marta <sorinmarta0@gmail.com>
2024-08-05 13:38:10 +0300

    Replace doAction with applyFilters

--- src/global/components/layout/Header.jsx
+++ src/global/components/layout/Header.jsx
@@ -1,4 +1,4 @@
-import { doAction } from '@wordpress/hooks';
+import { applyFilters } from '@wordpress/hooks';
 
 /**
  * Add the main Header for the pages
@@ -10,19 +10,49 @@ import { doAction } from '@wordpress/hooks';
  * @constructor
  */
 export const Header = ({ logoURL, logoLink, pageTitle }) => {
-	if (!logoURL) {
-		logoURL = 'https://tadamus.com';
-	}
+	const logoSite = logoURL ? logoURL : 'https://tadamus.com';
+
+	/**
+	 * Filter before the header logo
+	 *
+	 * @action licensehub.header-before-logo
+	 * @param {string} logoSite - The URL that should be applied to the logo.
+	 * @param {string} logoLink - The URL of the logo image.
+	 * @param {string} pageTitle - The title of the page.
+	 */
+	const headerBeforeLogo = applyFilters(
+		'licensehub.header-before-logo',
+		'',
+		logoSite,
+		logoLink,
+		pageTitle,
+	);
+
+	/**
+	 * Filter after the header logo
+	 *
+	 * @action licensehub.header-after-logo
+	 * @param {string} logoSite - The URL that should be applied to the logo.
+	 * @param {string} logoLink - The URL of the logo image.
+	 * @param {string} pageTitle - The title of the page.
+	 */
+	const headerAfterLogo = applyFilters(
+		'licensehub.header-after-logo',
+		'',
+		logoSite,
+		logoLink,
+		pageTitle,
+	);
 
 	return (
 		<div className="flex flex-row items-center mt-2">
-			{doAction('lchb-header-before-logo', logoURL, logoLink, pageTitle)}
+			{headerBeforeLogo}
 			<div className="border-4 border-black border-solid border-t-0 border-b-0 border-l-0 pr-2">
-				<a href={logoURL} target="_blank" rel="noreferrer">
+				<a href={logoSite} target="_blank" rel="noreferrer">
 					<img src={logoLink} alt="logo" width="200px" />
 				</a>
 			</div>
-			{doAction('lchb-header-after-logo', logoURL, logoLink, pageTitle)}
+			{headerAfterLogo}
 			<div className="pl-2">
 				<h1 className="font-kanit font-bold font-xl uppercase">{pageTitle}</h1>
 			</div>
--- src/global/components/table/Table.jsx
+++ src/global/components/table/Table.jsx
@@ -1,4 +1,4 @@
-import { doAction } from '@wordpress/hooks';
+import { applyFilters } from '@wordpress/hooks';
 import { TableHeader, TableRow } from '@global';
 
 /**
@@ -26,10 +26,66 @@ export const Table = ({
 }) => {
 	const defaultClasses = 'tada-table mt-3';
 
+	/**
+	 * Filter before the table head
+	 *
+	 * @action licensehub.before-table-head
+	 * @param {array} headers - The headers of the table.
+	 * @param {array} rows - The rows of the table.
+	 */
+	const beforeTableHead = applyFilters(
+		'licensehub.before-table-head',
+		'',
+		headers,
+		rows,
+	);
+
+	/**
+	 * Filter after the table head
+	 *
+	 * @action licensehub.after-table-head
+	 * @param {array} headers - The headers of the table.
+	 * @param {array} rows - The rows of the table.
+	 */
+	const afterTableHead = applyFilters(
+		'licensehub.after-table-head',
+		'',
+		headers,
+		rows,
+	);
+
+	/**
+	 * Filter before the table body
+	 *
+	 * @action licensehub.before-table-body
+	 * @param {array} headers - The headers of the table.
+	 * @param {array} rows - The rows of the table.
+	 */
+	const beforeTableBody = applyFilters(
+		'licensehub.before-table-body',
+		'',
+		headers,
+		rows,
+	);
+
+	/**
+	 * Filter after the table body
+	 *
+	 * @action licensehub.after-table-body
+	 * @param {array} headers - The headers of the table.
+	 * @param {array} rows - The rows of the table.
+	 */
+	const afterTableBody = applyFilters(
+		'licensehub.after-table-body',
+		'',
+		headers,
+		rows,
+	);
+
 	return (
 		<table
 			className={className ? defaultClasses + ' ' + className : defaultClasses}>
-			{doAction('lchb-table-before-head')}
+			{beforeTableHead}
 			<thead>
 				<tr>
 					{headers.map((header, index) => (
@@ -37,9 +93,9 @@ export const Table = ({
 					))}
 				</tr>
 			</thead>
-			{doAction('lchb-table-after-head')}
+			{afterTableHead}
 			<tbody>
-				{doAction('lchb-table-before-body')}
+				{beforeTableBody}
 				{rows.map((row, index) => (
 					<TableRow
 						columns={row}
@@ -50,7 +106,7 @@ export const Table = ({
 						{...props}
 					/>
 				))}
-				{doAction('lchb-table-after-body')}
+				{afterTableBody}
 			</tbody>
 		</table>
 	);
